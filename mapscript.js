7f5d91324e5195e2688fdb51f6dcf659a0889d62
Spekulativ <148905611+Spekulativ@users.noreply.github.com>
2024-01-08 02:14:14 +0100

    Tried example with custom image for icons

--- mapscript.js
+++ mapscript.js
@@ -24,13 +24,34 @@ map.locate({ setView: true, maxZoom: 16 });
 
 //Implementation of geoJSON
 
+//test
 
-//1 - Ringkøbing-Skjern
+function createCustomIcon (feature, latlng) {
+  let myIcon = L.icon({
+    iconUrl: 'disabled-toilet-handicap-svg.png',
+    shadowUrl: 'disabled-toilet-handicap-svg.png',
+    iconSize:     [25, 25], // width and height of the image in pixels
+    shadowSize:   [35, 20], // width, height of optional shadow image
+    iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+    shadowAnchor: [12, 6],  // anchor point of the shadow. should be offset
+    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+  })
+  return L.marker(latlng, { icon: myIcon })
+}
 
-L.geoJson(dataRing).addTo(map);
+// create an options object that specifies which function will called on each feature
+let myLayerOptions = {
+  pointToLayer: createCustomIcon
+}
+
+// create the GeoJSON layer
+L.geoJSON(dataRing, myLayerOptions).addTo(map)
+
+//fiseof
 
 
-L.geoJson(dataKK, options).addTo(map);
+
+L.geoJson(dataKK).addTo(map);
 
 //1 - Ringkøbing-Skjern
 
